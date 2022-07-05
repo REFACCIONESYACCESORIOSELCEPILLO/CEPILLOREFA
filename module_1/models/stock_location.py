fd62ec7a63b614f1f4a05446385e2c2d07445250
REFACCIONESYACCESORIOSELCEPILLO <106451052+REFACCIONESYACCESORIOSELCEPILLO@users.noreply.github.com>
2022-07-05 13:11:53 -0500

    Custom module 1

---
+++ module_1/models/stock_location.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import logging
+from odoo import api, models, fields, _
+
+_logger = logging.getLogger(__name__)
+
+
+class StockLocation(models.Model):
+    _inherit = 'stock.location'
+
+    branch = fields.Selection(
+        selection=[('MAN', 'Manantial'), 
+                   ('MAG', 'Magon'),
+                   ('POZ', 'Poza Rica'),
+                   ('PAP', 'Papantla'),
+                   ('TUX', 'Tuxpan'),],
+        string=_('Sucursal'), required=True,
+    )
+
