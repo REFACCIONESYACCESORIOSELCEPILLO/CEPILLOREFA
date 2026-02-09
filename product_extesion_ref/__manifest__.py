d6646bdc0df2f8854e5c1c7c0f52bf83940d5794
Israel Cuellar <sistemas@tendenciainterior.com>
2026-02-09 14:40:08 -0600

    [FIX] manifest syntax product_extesion_ref

--- product_extesion_ref/__manifest__.py
+++ product_extesion_ref/__manifest__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 {
-    'name': 'Extención de productos y relación',
-    'version': '18.0.1.0.0,
-    'description': '',
+    'name': 'Extensión de productos y relación',
+    'version': '18.0.1.0.0',
+    'summary': 'Extensión de productos y relación',
+    'description': 'Extensión de productos y relación',
     'author': 'Gabriel Lopez',
     'license': 'LGPL-3',
-    'category': 'product',
+    'category': 'Product',
     'depends': [
         'stock',
         'sale',
@@ -18,12 +18,11 @@
         'views/product_template_view.xml',
         'views/templates.xml',
     ],
-    # 'auto_install': True,
     'application': False,
     'installable': True,
     'assets': {
         'web.assets_frontend': [
-            'product_extesion_ref/static/src/js/website_sale.js'
+            'product_extension_ref/static/src/js/website_sale.js',
         ],
     },
 }
