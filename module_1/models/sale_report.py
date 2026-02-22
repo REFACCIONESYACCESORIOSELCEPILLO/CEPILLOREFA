33ab33f0fdc91d916d9f7a4d9cae875e1216b143
Israel Cuellar <sistemas@tendenciainterior.com>
2026-02-21 23:50:30 -0600

    Fix sale.report UNION for Odoo.sh_1

--- module_1/models/sale_report.py
+++ module_1/models/sale_report.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 
 class SaleReport(models.Model):
-    _inherit = 'sale.report'
+    _inherit = "sale.report"
 
     suc_manantial = fields.Float(readonly=True)
     suc_magon = fields.Float(readonly=True)
@@ -10,41 +10,18 @@ class SaleReport(models.Model):
     suc_papantla = fields.Float(readonly=True)
     suc_tuxpan = fields.Float(readonly=True)
 
-    # ---------------- SALE ----------------
+    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
+        fields['suc_manantial'] = ", CASE WHEN sl.branch = 'MAN' THEN 1 ELSE 0 END"
+        fields['suc_magon'] = ", CASE WHEN sl.branch = 'MAG' THEN 1 ELSE 0 END"
+        fields['suc_poza_rica'] = ", CASE WHEN sl.branch = 'POZ' THEN 1 ELSE 0 END"
+        fields['suc_papantla'] = ", CASE WHEN sl.branch = 'PAP' THEN 1 ELSE 0 END"
+        fields['suc_tuxpan'] = ", CASE WHEN sl.branch = 'TUX' THEN 1 ELSE 0 END"
 
-    def _select_sale(self):
-        res = super()._select_sale()
-        res += """
-            ,CASE WHEN sl.branch = 'MAN' THEN 1 ELSE 0 END AS suc_manantial
-            ,CASE WHEN sl.branch = 'MAG' THEN 1 ELSE 0 END AS suc_magon
-            ,CASE WHEN sl.branch = 'POZ' THEN 1 ELSE 0 END AS suc_poza_rica
-            ,CASE WHEN sl.branch = 'PAP' THEN 1 ELSE 0 END AS suc_papantla
-            ,CASE WHEN sl.branch = 'TUX' THEN 1 ELSE 0 END AS suc_tuxpan
-        """
-        return res
-
-    def _from_sale(self):
-        res = super()._from_sale()
-        res += """
+        from_clause += """
             LEFT JOIN stock_warehouse sh ON sh.id = s.warehouse_id
             LEFT JOIN stock_location sl ON sl.id = sh.lot_stock_id
         """
-        return res
 
-    def _group_by_sale(self):
-        res = super()._group_by_sale()
-        res += ", sl.branch"
-        return res
+        groupby += ", sl.branch"
 
-    # ---------------- POS (FIX CRÍTICO) ----------------
-
-    def _select_pos(self):
-        res = super()._select_pos()
-        res += """
-            ,0 AS suc_manantial
-            ,0 AS suc_magon
-            ,0 AS suc_poza_rica
-            ,0 AS suc_papantla
-            ,0 AS suc_tuxpan
-        """
-        return res
+        return super()._query(with_clause, fields, groupby, from_clause)
